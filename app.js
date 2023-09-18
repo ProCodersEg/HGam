d7298ce30ade61be905d3b745ce2d9e05400308a
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2023-09-19 02:24:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,32 @@
+// Initialize Firebase (make sure to include the Firebase SDK)
+firebase.initializeApp({
+    apiKey: 'AIzaSyCRswv006Mon_V7TEHL_kVoX10QihbQ24Y',
+    authDomain: 'saraha-5e7fd.firebaseapp.com',
+    projectId: '1:942089850758:android:34cd2e9ab9ba5cee4e2607'
+});
 
+// Initialize Firestore
+const db = firebase.firestore();
 
-// Initialize Firebase with your config
-const firebaseConfig = {
-   apiKey: "AIzaSyBEtXBiFXnk3jPmb6wdfW4SOvMXJQIjCzg",
-    authDomain: "saraha-5e7fd.firebaseapp.com",
-    projectId: "saraha-5e7fd",
-    storageBucket: "saraha-5e7fd.appspot.com",
-    messagingSenderId: "942089850758",
-    appId: "1:942089850758:web:bc767461bc426a904e2607"
-};
+// Get the user ID from the URL or another source
+const userId = 'WXRbWaH3SnNlD7xUEoIy5PhQaIS2';
 
-firebase.initializeApp(firebaseConfig);
-const firestore = firebase.firestore();
+// Reference to the user document
+const userRef = db.collection('users').doc(userId);
 
-// Fetch user data
-const userId = "WXRbWaH3SnNlD7xUEoIy5PhQaIS2"; // Replace with the user's ID
-const userRef = firestore.collection("users").doc(userId);
-
+// Get user data from Firestore
 userRef.get().then((doc) => {
     if (doc.exists) {
         const data = doc.data();
-        document.getElementById("userName").textContent = data.name;
-        document.getElementById("profilePhoto").src = data.photoURL;
+        const userPhoto = document.getElementById('userPhoto');
+        const userName = document.getElementById('userName');
+
+        // Set the photo and name in the HTML
+        userPhoto.src = data.photoURL;
+        userName.textContent = data.name;
     } else {
-        console.log("User not found");
+        console.log('No such document!');
     }
-});
-
-// Send a message
-const sendMessageButton = document.getElementById("sendMessageButton");
-sendMessageButton.addEventListener("click", () => {
-    const messageInput = document.getElementById("messageInput").value;
-    const message = {
-        senderId: "942089850758",
-        receiverId: userId,
-        message: messageInput,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    };
-
-    firestore.collection("messages").add(message)
-        .then(() => {
-            console.log("Message sent successfully");
-            document.getElementById("messageInput").value = "";
-        })
-        .catch((error) => {
-            console.error("Error sending message: ", error);
-        });
+}).catch((error) => {
+    console.log('Error getting document:', error);
 });
