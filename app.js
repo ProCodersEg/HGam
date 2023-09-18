3fd351978a56141a579a25c35386b5c845995487
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2023-09-19 01:52:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// Initialize Firebase with your Firebase project config
+firebase.initializeApp({
+  apiKey: "AIzaSyBEtXBiFXnk3jPmb6wdfW4SOvMXJQIjCzg",
+  authDomain: "saraha-5e7fd.firebaseapp.com",
+  projectId: "saraha-5e7fd",
+  storageBucket: "saraha-5e7fd.appspot.com",
+  messagingSenderId: "942089850758",
+  appId: "1:942089850758:web:bc767461bc426a904e2607"
+});
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+// Replace 'YOUR_COLLECTION_NAME' with the name of your Firestore collection
+const usersCollection = db.collection('users');
+
+// Function to display user profile information
+function displayUserProfile(userId) {
+    usersCollection.doc(userId).get().then((doc) => {
+        if (doc.exists) {
+            const data = doc.data();
+            document.getElementById('profile-photo').src = data.photoUrl; // Replace with the actual field name for the photo URL
+            document.getElementById('user-name').textContent = data.name; // Replace with the actual field name for the user's name
+        } else {
+            console.log('User not found');
+        }
+    }).catch((error) => {
+        console.error('Error getting user profile:', error);
+    });
+}
+
+// Function to send a message
+document.getElementById('send-button').addEventListener('click', () => {
+    const message = document.getElementById('message-input').value;
+    const userId = 'WXRbWaH3SnNlD7xUEoIy5PhQaIS2'; // Replace with the actual user's ID
+
+    // Update the 'secret' field with the new message
+    usersCollection.doc(userId).update({
+        secret: message
+    }).then(() => {
+        console.log('Message sent successfully');
+    }).catch((error) => {
+        console.error('Error sending message:', error);
+    });
+});
+
+// Call the function to display the user's profile (you need to replace 'USER_ID' with the actual user's ID)
+displayUserProfile('WXRbWaH3SnNlD7xUEoIy5PhQaIS2');
